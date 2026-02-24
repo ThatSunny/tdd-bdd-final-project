8633be1e611894b03bd20db6cca1a22b5c2fcddd
Sunny <sunny.22scse1012788@galgotiasuniversity.edu.in>
2026-02-24 16:23:30 -0500

    Implement REST API endpoints: READ, UPDATE, DELETE, LIST and query filters

--- service/routes.py
+++ service/routes.py
@@ -1,27 +1,14 @@
 ######################################################################
 # Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 ######################################################################
 
-# spell: ignore Rofrano jsonify restx dbname
 """
 Product Store Service with UI
 """
 from flask import jsonify, request, abort
-from flask import url_for  # noqa: F401 pylint: disable=unused-import
+from flask import url_for
 from service.models import Product
-from service.common import status  # HTTP Status Codes
+from service.common import status
 from . import app
 
 
@@ -49,20 +36,14 @@ def index():
 def check_content_type(content_type):
     """Checks that the media type is correct"""
     if "Content-Type" not in request.headers:
-        app.logger.error("No Content-Type specified.")
-        abort(
-            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            f"Content-Type must be {content_type}",
-        )
+        abort(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+              f"Content-Type must be {content_type}")
 
     if request.headers["Content-Type"] == content_type:
         return
 
-    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {content_type}",
-    )
+    abort(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+          f"Content-Type must be {content_type}")
 
 
 ######################################################################
@@ -70,59 +51,100 @@ def check_content_type(content_type):
 ######################################################################
 @app.route("/products", methods=["POST"])
 def create_products():
-    """
-    Creates a Product
-    This endpoint will create a Product based the data in the body that is posted
-    """
-    app.logger.info("Request to Create a Product...")
+    """Creates a Product"""
     check_content_type("application/json")
 
     data = request.get_json()
-    app.logger.info("Processing: %s", data)
     product = Product()
     product.deserialize(data)
     product.create()
-    app.logger.info("Product with new id [%s] saved!", product.id)
 
-    message = product.serialize()
+    location_url = url_for("get_products",
+                           product_id=product.id,
+                           _external=False)
 
-    #
-    # Uncomment this line of code once you implement READ A PRODUCT
-    #
-    # location_url = url_for("get_products", product_id=product.id, _external=True)
-    location_url = "/"  # delete once READ is implemented
-    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    return jsonify(product.serialize()), \
+           status.HTTP_201_CREATED, \
+           {"Location": location_url}
 
 
 ######################################################################
 # L I S T   A L L   P R O D U C T S
 ######################################################################
+@app.route("/products", methods=["GET"])
+def list_products():
+    """Returns all Products"""
+    from service.models import Category
 
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
+
+    if name:
+        products = Product.find_by_name(name)
+
+    elif category:
+        try:
+            category_enum = Category[category]
+        except KeyError:
+            abort(status.HTTP_400_BAD_REQUEST,
+                  f"Category {category} is not valid.")
+        products = Product.find_by_category(category_enum)
+
+    elif available:
+        available_bool = available.lower() == "true"
+        products = Product.find_by_availability(available_bool)
+
+    else:
+        products = Product.all()
+
+    results = [product.serialize() for product in products]
+    return jsonify(results), status.HTTP_200_OK
+
 
 ######################################################################
 # R E A D   A   P R O D U C T
 ######################################################################
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_products(product_id):
+    """Retrieve a single Product"""
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Product with id '{product_id}' was not found.")
 
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
+    return jsonify(product.serialize()), status.HTTP_200_OK
+
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
 ######################################################################
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """Update an existing Product"""
+    check_content_type("application/json")
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Product with id '{product_id}' was not found.")
+
+    data = request.get_json()
+    product.deserialize(data)
+    product.id = product_id
+    product.update()
+
+    return jsonify(product.serialize()), status.HTTP_200_OK
+
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
 ######################################################################
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """Delete a Product"""
+    product = Product.find(product_id)
+    if product:
+        product.delete()
 
-
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
+    return "", status.HTTP_204_NO_CONTENT
